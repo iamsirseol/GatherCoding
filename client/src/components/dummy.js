0d51395adafa4b4ceae0de0ada537035b4f13ebf
june <ryu9663@naver.com>
2021-11-11 16:17:28 +0900

    practice

---
+++ client/src/components/dummy.js
@@ -0,0 +1,18 @@
+export const groups = [
+    {
+        title : '청주 모각코',
+        population : 8,
+        currentPopulation : 2,
+        gather_location : '상당산성 꼭대기',
+        leader : '류준열',
+        location_address : '청주'
+    },
+    {
+        title : '김천 모각코',
+        population : 3,
+        currentPopulation : 2,
+        gather_location : '김천터미널',
+        leader : '설동혁',
+        location_address : '김천'
+    }
+]
---
+++ client/src/pages/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <sidebar>
+                    <button>내모임</button>
+                    <button>모각코 구하기</button>
+                    <button>지역 설정</button>
+                    <button>TIL 작성하기</button>
+                </sidebar>
+    )
+}
+
+export default Sidebar
