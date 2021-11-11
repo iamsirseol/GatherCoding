5231f9cd384cc0b45ec628e77f03d1e06a24cf2b
LeeYhunHwan <yhlee1227@naver.com>
2021-11-11 16:40:24 +0900

    landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
-import './reset.css';
-import './css/homePage.css';
-
-import Home from './pages/home';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Home />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
+// import './reset.css';
+// import './css/homePage.css';
+
+// import Home from './pages/home';
+
+// function App() {
+//   return (
+//     <div>
+//       <Home />
+//     </div>
+//   )
+// }
+
+// export default App;
