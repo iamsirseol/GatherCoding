5810d2128214f65e8c0b1096ad5849a532136618
june <ryu9663@naver.com>
2021-11-11 20:55:00 +0900

    1. fix: fix  manifext err, 2. refactor : Header.js, Sidebar.js, 3. feat :  function logoutHandler

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -2,28 +2,27 @@ import React from 'react'
 import '../css/homeComponent.css'
 import Header from '../pages/Header'
 import Sidebar from '../pages/Sidebar'
-import { groups } from './dummy'
+import { groups, userInfo } from './dummy'
 function HomeComponent() {
     return (
         <div className = 'page'>
             
-            <Header />
+            {/* <Header /> */}
             <div className = 'body'>
                 
-                <Sidebar />
+                {/* <Sidebar /> */}
                 <div className = 'room-list'>
                     {/* 유저의 위치로 바꿔야할듯 */}
-                    <div className = 'room-location'> {groups[0].location_address}에 있는 모각코 모임</div>
-                    {groups
-                        .filter(el=>el.location_address==='청주')
+                    <div className = 'room-location'> {userInfo.username}님이 참가하고 계신 모각코 모임입니다.</div>
+                    {userInfo.joinGroup
                         .map((group,idx) => {
                         return (
                             
                                 <button className = 'room' key = {idx}>
-                                <div>방 이름 : {group.title}</div>
-                                <div>인원 : {group.currentPopulation}/{group.population}</div>
-                                <div>코딩 장소 : {group.gather_location}</div>
-                                <div>리더 : {group.leader}</div>
+                                    <div>방 이름 : {group.title}</div>
+                                    <div>인원 : {group.currentPopulation}/{group.population}</div>
+                                    <div>코딩 장소 : {group.gather_location}</div>
+                                    <div>리더 : {group.leader}</div>
                                 </button>
                         )
                     })}
