5a4422abf104ff87ad4da69114c880a67e79b079
june <ryu9663@naver.com>
2021-11-14 22:18:13 +0900

    [refactor] HomeLogined.js랑 RoomListPage.js에서 공통되는 부분 <Room />로 리팩토링

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import { groups } from '../components/dummy'
-import { useSelector} from 'react-redux';
-// import '../css/homeLogined.css';
-function Room() {
-    const {city} = useSelector(state=>state.locationReducer)
+
+
+import '../css/room.css';
+function Room({onClick, group, idx}) {
+    
     return (
     <>
-
-    {groups.filter(el=>el.location_address.split(' ')[1] === city)
-
-
-
-    .map((group,idx) => {
-        return (
-            <button className = 'roomListPage-room' key = {idx}>
+            <button onClick = {onClick} className = 'roomListPage-room' key = {idx}>
                 <div>방 이름 : {group.title}</div>
                 <div>인원 : {group.currentPopulation}/{group.population}</div>
                 <div>코딩 장소 : {group.gather_location}</div>
                 <div>리더 : {group.leader}</div>
-            </button>
-            )
-        })}
+            </button>  
     </>      
     )
 }
