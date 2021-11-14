478ebbd4d3b3f743db975218301b38d3d3226674
yy5143 <yys171@naver.com>
2021-11-14 14:54:18 +0900

    [modify] 라우터 및 데이터베이스 수정

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,11 +1,87 @@
-const { user } = require('../models');
+// // const { BelongsTo } = require('sequelize/types');
+// // const { DESCRIBE } = require('sequelize/types/lib/query-types');
+// const { user, group } = require('../models');
 
+
+// module.exports = {
+//     post: (req, res) => {
+
+//         res.send("Hello World");
+//     },
+//     get: async (req, res) => {
+
+//         const jane = await user.create({
+//             username: 'Jane',
+//             email: 'assdf@gmail.com',
+//             password: '1234',
+//             image: 'dgasgs',
+//             blog: 'sgdsgsa',
+//             current_location: 'Seoul'
+//         });
+//         const mogako = await group.create({
+//             title: 'mogako seoul',
+//             describe: 'gather in seoul',
+//             population: 8,
+//             gather_location: 'Seoul Cafe',
+//             leader: 1,
+//             location_address: 'Seoul'
+//         })
+//         await jane.addGroup(mogako)
+//         .then((result) => {
+//             console.log('success');
+//         })
+//         const jane_group = group.findAll({
+//             include: [{
+//                 model: user,
+//                 where: {
+//                     username: 'Jane'
+//                 }
+//             }]
+//         })
+//         .then((result) => {
+//             console.log(result);
+//             res.send(result);
+//         });
+//         // res.send("Hello World");
+//     }    
+// };
+
+const { User, Group } = require('../models');
 
 module.exports = {
     post: (req, res) => {
+
         res.send("Hello World");
     },
-    get: (req, res) => {
-        res.send("Hello World");
-    }    
+    get: async (req, res) => {
+        // const amidala = await User.create({ username: 'p4dm3', points: 1000 });
+        // const queen = await Profile.create({ name: 'Queen' });
+        // await amidala.addProfile(queen, { through: { selfGranted: false } });
+        const jane = await User.create({ 
+            username: 'Jane',
+            password: '4567'
+        });
+        const mogako = await Group.create({
+            title: 'mogako',
+            location: 'Seoul'
+        });
+        await jane.addGroup(mogako)
+        .then((result) => {
+            console.log('success');
+        })
+        const jane_group = Group.findAll({
+            include: [{
+                model: User,
+                where: {
+                    username: 'Jane'
+                }
+            }],
+        })
+        .then((result) => {
+            console.log(result);
+            res.send(result);
+        })
+
+        // res.send('success')
+    }
 };
